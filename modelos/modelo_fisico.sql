136d23b74a8188c89b25239c119aff5b2781957e
Gabriel Viggiano <gviggianofonseca@gmail.com>
2019-05-13 18:05:49 -0300

    Update modelo_fisico.sql

--- modelos/modelo_fisico.sql
+++ modelos/modelo_fisico.sql
@@ -1,31 +1,11 @@
 CREATE TABLE Paciente (
 ID_Paciente INT NOT NULL,
-CPF_Paciente INT NOT NULL,
+CPF_Paciente BIGINT,
 NM_Paciente VARCHAR(100),
 Email_Paciente VARCHAR(50),
-Tel_Paciente INT NOT NULL,
+Tel_Paciente BIGINT,
 Senha_Paciente VARCHAR(25),
 PRIMARY KEY(ID_Paciente)
-);
-
-CREATE TABLE Exame_Paciente (
-CD_Exame_Paciente INT NOT NULL,
-CPF_Paciente INT NOT NULL,
-CD_Exame INT NOT NULL,
-CRM_Medico VARCHAR(15),
-DT_Exame DATE,
-Realizado BOOLEAN,
-PRIMARY KEY (CD_Exame_Paciente),
-FOREIGN KEY (CD_Exame) REFERENCES Exame (CD_Exame),
-FOREIGN KEY (CRM_Medico) REFERENCES Medico (CRM_Medico),
-FOREIGN KEY (CPF_Paciente) REFERENCES Paciente (CPF_Paciente)
-);
-
-CREATE TABLE Exame (
-CD_Exame INT NOT NULL,
-Tipo_Exame VARCHAR(30),
-Preco FLOAT,
-PRIMARY KEY (CD_Exame)
 );
 
 CREATE TABLE Medico (
@@ -36,15 +16,35 @@ Especialidade_Medico VARCHAR(30),
 PRIMARY KEY (CRM_Medico)
 );
 
+CREATE TABLE Exame (
+CD_Exame INT NOT NULL,
+Tipo_Exame VARCHAR(30),
+Preco FLOAT,
+PRIMARY KEY (CD_Exame)
+);
+
 CREATE TABLE Unidade (
 CD_Unidade INT NOT NULL,
 Logradouro VARCHAR(100),
 Bairro VARCHAR(50),
 Complemento VARCHAR(100),
 Cidade VARCHAR(50),
-CEP INT NOT NULL,
+CEP BIGINT,
 Numero INT NOT NULL,
-Tel INT NOT NULL,
+Tel BIGINT,
 
 PRIMARY KEY (CD_Unidade)
+);
+
+CREATE TABLE Exame_Paciente (
+CD_Exame_Paciente INT NOT NULL,
+ID_Paciente INT NOT NULL,
+CD_Exame INT NOT NULL,
+CRM_Medico VARCHAR(15),
+DT_Exame DATE,
+Realizado BOOLEAN,
+PRIMARY KEY (CD_Exame_Paciente),
+FOREIGN KEY (CD_Exame) REFERENCES Exame (CD_Exame),
+FOREIGN KEY (CRM_Medico) REFERENCES Medico (CRM_Medico),
+FOREIGN KEY (ID_Paciente) REFERENCES Paciente (ID_Paciente)
 );
